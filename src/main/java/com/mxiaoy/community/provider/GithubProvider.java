f01be2ef8018178ed077317f583d1202cad6f522
mxiaoy27 <mxiaoy27@qq.com>
2020-01-03 18:11:03 +0800

    space

--- src/main/java/com/mxiaoy/community/provider/GithubProvider.java
+++ src/main/java/com/mxiaoy/community/provider/GithubProvider.java
@@ -41,18 +41,17 @@ public class GithubProvider {
     public GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
 
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
 
-            Request request = new Request.Builder()
-                    .url("https://api.github.com/user?access_token="+accessToken)
-                    .build();
-
-            try (Response response = client.newCall(request).execute()) {
-                String str = response.body().string();
-                GithubUser githubUser = JSON.parseObject(str, GithubUser.class);
-                return githubUser;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try (Response response = client.newCall(request).execute()) {
+            String str = response.body().string();
+            GithubUser githubUser = JSON.parseObject(str, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
