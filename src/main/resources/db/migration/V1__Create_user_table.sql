76086c286de0a32d768dc0225c80673ceede2b42
mxiaoy27 <mxiaoy27@qq.com>
2020-01-10 17:10:32 +0800

    delete table users，then create it by flyway。

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_id` varchar(100),
+  `user_name` varchar(50),
+  `user_token` char(36),
+  `gmt_create` bigint(20),
+  `gmt_modified` bigint(20),
+  PRIMARY KEY (`id`) USING BTREE
+);
